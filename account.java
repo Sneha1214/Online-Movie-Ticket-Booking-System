3c3f27646dc8b1873b5c0353fbf79ed002c49fcb
Sneha Bhaskara <71217277+Sneha1214@users.noreply.github.com>
2022-04-29 09:50:44 +0530

    Add files via upload

---
+++ account.java
@@ -0,0 +1,18 @@
+/*
+
+ */
+package online_movie_ticket_booking_system;
+
+/**
+ *
+ 
+ */
+public class account {
+    String Email = null;
+    int Type;
+    String Name;
+    String Gender;
+    int id;
+    Float wallet_amt;
+        
+}
